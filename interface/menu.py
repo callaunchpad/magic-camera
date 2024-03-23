0f649eda9b3f84588da2bfee999ee0fdd4782a57
Michelle Pan <michellepan8@gmail.com>
2024-03-23 01:22:56 -0700

    Add text and multiple buttons

--- interface/menu.py
+++ interface/menu.py
@@ -1,12 +1,17 @@
-from PIL import ImageFont
+from PIL import ImageFont, ImageDraw
+from typing import Sequence
 
-FNT = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 30)
+FNT = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
 
 ITEM_OUTLINE = "#FFFFFF"
 
+ITEM_HEIGHT = 40
+ITEM_X_MARGIN = 10
+ITEM_Y_MARGIN = 10
+
 class Menu:
 
-    def __init__(self, image_draw, modes):
+    def __init__(self, image_draw: ImageDraw, modes: Sequence[str]):
         assert len(modes) > 0, "must have at least one mode"
         self.image_draw = image_draw
         self.modes = modes
@@ -19,5 +24,15 @@ class Menu:
         self.mode_id = max(self.mode_id - 1, 0)
 
     def draw(self):
-        self.image_draw.rectangle((10, 10, 230, 50), outline=ITEM_OUTLINE)
+        for i, mode in enumerate(self.modes):
+            self.image_draw.rectangle(
+                xy=(ITEM_X_MARGIN, ITEM_Y_MARGIN + i * (ITEM_HEIGHT + ITEM_Y_MARGIN), 240 - ITEM_X_MARGIN, (i+1) * (ITEM_HEIGHT + ITEM_Y_MARGIN)),
+                outline=ITEM_OUTLINE,
+            )
+            self.image_draw.text(
+                xy=(ITEM_X_MARGIN + 5, 5 + ITEM_Y_MARGIN + i * (ITEM_HEIGHT + ITEM_Y_MARGIN)),
+                text="Hello World",
+                font=FNT,
+                fill="#ffffff",
+            )
 
