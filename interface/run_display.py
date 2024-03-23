13717edd7c9bde0bf1103fc29f8e70b3873aef10
Michelle Pan <michellepan8@gmail.com>
2024-03-23 16:46:31 -0700

    Clean up code

--- interface/run_display.py
+++ interface/run_display.py
@@ -1,5 +1,3 @@
-import time
-
 from display import Display
 
 display = Display(modes = ["mode one", "mode two", "mode three", "mode four", "mode five"])
--- interface/viewfinder.py
+++
@@ -1,7 +0,0 @@
-class Viewfinder:
-
-    def __init__(self, image_draw):
-        self.image_draw = image_draw
-
-    def draw(self):
-        pass
