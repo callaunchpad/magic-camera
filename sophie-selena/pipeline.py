c7955a7eaef44e259a38b1b5e8a6c4a4ba9bd45d
sophie200 <sophiexe60@gmail.com>
2024-04-22 17:29:48 -0700

    add deconstruction phase

--- sophie-selena/pipeline.py
+++ sophie-selena/pipeline.py
@@ -7,6 +7,31 @@ import os
 import base64
 from io import BytesIO
 
+# Some basic setup:
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import sys
+import numpy as np
+import os, json, cv2, random
+from google.colab.patches import cv2_imshow
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+# Detic libraries
+sys.path.insert(0, 'third_party/CenterNet2/')
+from centernet.config import add_centernet_config
+from detic.config import add_detic_config
+from detic.modeling.utils import reset_cls_test
+
 def preprocess(image):
   img = Image.open(image)
   return img
@@ -92,6 +117,55 @@ class Predictor(BasePredictor):
         num_inference_steps: int = 10,
   ) -> Path:
     """Run a single prediction on the model"""
+
+    #--- DECONSTRUCTION PHASE ---#
+    # Build the detector and download our pretrained weights
+    cfg = get_cfg()
+    add_centernet_config(cfg)
+    add_detic_config(cfg)
+    cfg.merge_from_file("configs/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.yaml")
+    cfg.MODEL.WEIGHTS = 'https://dl.fbaipublicfiles.com/detic/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth'
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+    cfg.MODEL.ROI_BOX_HEAD.ZEROSHOT_WEIGHT_PATH = 'rand'
+    cfg.MODEL.ROI_HEADS.ONE_CLASS_PER_PROPOSAL = True # For better visualization purpose. Set to False for all classes.
+    # cfg.MODEL.DEVICE='cpu' # uncomment this to use cpu-only mode.
+    predictor = DefaultPredictor(cfg)
+
+    # Setup the model's vocabulary using build-in datasets
+
+    BUILDIN_CLASSIFIER = {
+        'lvis': 'datasets/metadata/lvis_v1_clip_a+cname.npy',
+        'objects365': 'datasets/metadata/o365_clip_a+cnamefix.npy',
+        'openimages': 'datasets/metadata/oid_clip_a+cname.npy',
+        'coco': 'datasets/metadata/coco_clip_a+cname.npy',
+    }
+
+    BUILDIN_METADATA_PATH = {
+        'lvis': 'lvis_v1_val',
+        'objects365': 'objects365_v2_val',
+        'openimages': 'oid_val_expanded',
+        'coco': 'coco_2017_val',
+    }
+
+    vocabulary = 'lvis' # change to 'lvis', 'objects365', 'openimages', or 'coco'
+    metadata = MetadataCatalog.get(BUILDIN_METADATA_PATH[vocabulary])
+    classifier = BUILDIN_CLASSIFIER[vocabulary]
+    num_classes = len(metadata.thing_classes)
+    reset_cls_test(predictor.model, classifier, num_classes)
+
+    im = cv2.imread(image)
+    # Run model and show results
+    outputs = predictor(im)
+    v = Visualizer(im[:, :, ::-1], metadata)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+
+    #print([metadata.thing_classes[x] for x in outputs["instances"].pred_classes.cpu().tolist()]) # class names
+    #print(outputs["instances"].scores)
+    #print(outputs["instances"].pred_boxes.tensor.tolist())
+    phrases = [metadata.thing_classes[x] for x in outputs["instances"].pred_classes.cpu().tolist()]
+    boxes = outputs["instances"].pred_boxes.tensor.tolist()
+
+    #--- RECONSTRUCTION PHASE ---#
     processed_image = preprocess(image)
     # TODO take in processed_image
     boxes = [[0.1387, 0.2051, 0.4277, 0.7090], [0.4980, 0.4355, 0.8516, 0.7266]]
